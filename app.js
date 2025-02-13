cdf990ba0b5dad68406b61c75e9d5a2443f07ee8
MINSOO SHIN <minso0317@naver.com>
2025-02-13 20:47:45 +0900

    prisma이용헌 api호출

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    // where: { id: req.params.id }, // uuid를 사용하기 때문에 숫자열 형변환 없이 문자열로 사용한다.
+    where: { id },
+  });
+  res.send(user);
+});
+
+app.post("/users", async (req, res) => {
+  const user = await prisma.user.create({ data: req.body });
+  res.status(201).send(user);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: { id },
+  });
+  res.send("Success delete");
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+); // process.env로 포트를 띄우거나 혹시 없으면 PORT 3000 으로 띄워라
